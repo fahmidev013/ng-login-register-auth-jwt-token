197bf107a37132e980d599438e4df982a02f9429
fahmidev013 <flasherid@gmail.com>
2019-05-21 18:31:21 +0700

    add homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,25 @@
-﻿import { Component, OnInit, OnDestroy } from '@angular/core';
+﻿import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { first } from 'rxjs/operators';
 
 import { User } from '@app/_models';
 import { UserService, AuthenticationService } from '@app/_services';
+import { Router } from '@angular/router';
+
+import 'datatables.net-bs4';
+
+
+declare var $: any;
 
 @Component({ templateUrl: 'home.component.html' })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
     currentUser: User;
     currentUserSubscription: Subscription;
     users: User[] = [];
 
+
     constructor(
+        private router: Router,
         private authenticationService: AuthenticationService,
         private userService: UserService
     ) {
@@ -21,6 +29,10 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
+        this.loadScript('../../assets/js/jquery.slimscroll.js');
+        this.loadScript('../../assets/js/dropdown-bootstrap-extended.js');
+        this.loadScript('../../assets/js/dataTables-data.js');
+        this.loadScript('../../assets/js/toggle-data.js');
         this.loadAllUsers();
     }
 
@@ -28,6 +40,13 @@ export class HomeComponent implements OnInit, OnDestroy {
         // unsubscribe to ensure no memory leaks
         this.currentUserSubscription.unsubscribe();
     }
+
+    ngAfterViewInit() {
+        $(window).ready(function () {
+            $(".preloader-it").delay(500).fadeOut("slow");
+        });
+    }
+
 
     deleteUser(id: number) {
         this.userService.delete(id).pipe(first()).subscribe(() => {
@@ -39,5 +58,21 @@ export class HomeComponent implements OnInit, OnDestroy {
         this.userService.getAll().pipe(first()).subscribe(users => {
             this.users = users;
         });
+    }
+
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
+
+    public loadScript(url: string) {
+        const body = <HTMLDivElement>document.body;
+        const script = document.createElement('script');
+        script.innerHTML = '';
+        script.src = url;
+        script.async = true;
+        script.defer = false;
+        body.appendChild(script);
     }
 }
